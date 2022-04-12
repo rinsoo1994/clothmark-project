2a3658e6a12c439ba08fe74963006c4e3a1282b7
rinsoo <rinsoo1994@gmail.com>
2022-04-12 22:21:19 +0900

    Fix: useless console.log

--- server/api/controllers/cloth/cloth-mark-controller.ts
+++ server/api/controllers/cloth/cloth-mark-controller.ts
@@ -11,8 +11,10 @@ import { ShoppingSuccessResponse } from '../../models/shopping-response';
 @Route('cloth')
 export class ClothMarkController extends BaseController {
     @Get('all')
-    async getClothMarkAll(@Request() request: express.Request, @Query() user: string): Promise<ShoppingSuccessResponse<ClothMarkViewModel[]>> {
-        console.log(user);
+    async getClothMarkAll(
+        @Request() request: express.Request,
+        @Query() user: string
+    ): Promise<ShoppingSuccessResponse<ClothMarkViewModel[]>> {
         const data = await clothService.getClothMarkInfo(request, user);
         //  구조가 이상하긴 함. list 형태이고싶으면 안쪽에서 만들어주는것? 필요한가? 
         const shoppingSuccessResponse = new ShoppingSuccessResponse(data);
@@ -20,7 +22,10 @@ export class ClothMarkController extends BaseController {
     }
 
     @Post('info')
-    async putClothMarkInfo(@Body() requestBody: ClothMarkRequest, @Request() request: express.Request): Promise<ShoppingSuccessResponse<ClothMarkRequest>> {
+    async putClothMarkInfo(
+        @Body() requestBody: ClothMarkRequest,
+        @Request() request: express.Request
+    ): Promise<ShoppingSuccessResponse<ClothMarkRequest>> {
         const data = await clothService.putClothMarkInfo(request, requestBody);
         const shoppingSuccessResponse = new ShoppingSuccessResponse(data);
         return shoppingSuccessResponse;
--- server/index.ts
+++ server/index.ts
@@ -10,6 +10,7 @@ app.use(bodyParser.urlencoded({
     extended: true,
 })
 );
+
 app.use(bodyParser.json())
 
 RegisterRoutes(app);
